874603bd203281049b917e9781f823215b21f774
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-01-16 19:32:00 +0900

    ✨ add @nestjs/swagger for API documentation and configure Swagger setup

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,24 +1,20 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
   const logger = new Logger('bootstrap');
 
   const isProduction = configService.get('NODE_ENV') === 'production';
+
+  const config = new DocumentBuilder().setTitle('선린투데이 API').build();
 
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
@@ -29,6 +25,10 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  const swaggerEndpoint = configService.get('SWAGGER_ENDPOINT');
+  SwaggerModule.setup(swaggerEndpoint, app, documentFactory);
 
   await app.listen(
     isProduction ? Number(configService.get('PORT') || 3000) : 8000,
